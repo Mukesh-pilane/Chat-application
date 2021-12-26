6e8578f5033649816181e7a73e7a7889dc681b92
Mukesh-pilane <mukeshpilane@gmail.com>
2021-12-27 02:33:19 +0530

    version 2

--- public/script.js
+++ public/script.js
@@ -1,23 +1,57 @@
 const socket = io()
+
+
 let name;
+let room;
+let orignalHieght= window.innerHeight;
 let textarea = document.querySelector('#textarea')
 let messageArea = document.querySelector('.message__area')
-
+let body = document.querySelector('.chat__section')
+let stickers = document.querySelector('.stickerpad');
 do{
   name= prompt("Please Enter Ur Name")
 }while(!name)
+do{
+  room= prompt("Room name")
+}while(!room )
+window.onresize = dimensions;
 
- 
+function dimensions(){
+
+let stickerpadHeight;
+let dynamicHeight=window.innerHeight
+if(dynamicHeight!=orignalHieght){
+  stickerpadHeight=orignalHieght - dynamicHeight;
+  document.querySelector(":root").style.setProperty("--height",`${stickerpadHeight}px`)
+}
+}
+
+let userinfo ={
+  user:name,
+  room:room
+}
+
+socket.emit('join', userinfo);
+
+
 textarea.addEventListener("keyup", (e) => {
   if(e.key==="Enter"){
-    sendMessage(e.target.value)
+    sendMessage({msg:e.target.value,src:""})
   }
 })
 
+function send(){
+  sendMessage({msg: textarea.value,src:""});
+  textarea.setAttribute("autofocus", "true");
+  $("textarea").trigger("focus")
+}
+
 function sendMessage(message){
   let msg={
+    room:room,
     user:name,
-    message:message.trim()
+    message:message.msg.trim(),
+    src:message.src
   }
   appendMessage(msg, "outgoing")
   textarea.value = ''
@@ -29,20 +63,78 @@ function sendMessage(message){
 function appendMessage(msg, type){
   let mainDiv = document.createElement("div")
   let className = type
-  mainDiv.classList.add(className, 'message')
-  let markup = `
-   <h1>${msg.user}</h1>
+  mainDiv.classList.add(className, 'messages')
+  let markup =`<h1>${msg.user}</h1><p>${msg.message}</p>
+  <img src="${msg.src}"/>`
+  if(type=="outgoing"){
+    markup = `
+   <h1>You</h1>
    <p>${msg.message}</p>
+   <img src="${msg.src}"/>
   `
+  
+  }
+  
   mainDiv.innerHTML=markup
+  
   messageArea.appendChild(mainDiv)
 }
 
-socket.on('message', (msg) => {
+socket.on('message', msg => {
     appendMessage(msg, 'incoming')
     scrollToBottom()
-})
+});
+
+socket.on('join', info =>{
+  let joined= document.createElement("p");
+  joined.innerHTML=`<p>${info.user} as joined the chat</p>`
+  joined.classList.add("joiner")
+  messageArea.appendChild(joined)
+  scrollToBottom()
+});
+
+
+
+socket.on('saved-message', (data) => {
+    if(data.length){
+      data.forEach(msg =>{
+        if(msg.user!= name && msg.room == room){
+      appendMessage(msg, 'incoming')
+      scrollToBottom()
+        }else if(msg.room == room){
+         appendMessage(msg, 'outgoing')
+         scrollToBottom()
+        }
+     })
+scrollToBottom()
+    }else{
+      console.log("no chats")
+    }
+});
 
 function scrollToBottom() {
     messageArea.scrollTop = messageArea.scrollHeight
 }
+
+function hider(){
+  stickers.classList.add("d-none");
+}
+
+function stickpad(){
+ stickers.classList.remove("d-none");
+}
+
+var sticker = document.querySelectorAll(".sticker");
+for(var i = 0; i < sticker.length; i++){
+  sticker[i].addEventListener("click", function (){
+  sendMessage({msg:"", src:this.getAttribute('src')})
+    
+  });
+}
+
+/*document.querySelectorAll(".sticker").forEach((stickers) => {
+  stickers.addEventListener("click", () => {
+  console.log(this)
+})
+
+});*/
--- server.js
+++ server.js
@@ -1,7 +1,30 @@
 const express = require('express')
 const app = express()
 const http = require('http').createServer(app)
+const mongoose = require('mongoose')
+const URL ="mongodb+srv://mukeshpilane:123mukesh@cluster0.ut91y.mongodb.net/friendslog?retryWrites=true&w=majority"
 
+
+//mongodb connection and Schema
+mongoose.connect(URL, err => {
+  if(!err){
+    console.log("Connected")
+  }else{
+    console.log(err)
+  }
+});
+
+const messageSchema = mongoose.Schema({
+  room:String,
+  user: String,
+  message:String,
+  src:String
+}
+  );
+const Message = mongoose.model('Message', messageSchema);
+
+
+//Server connection
 const PORT = process.env.PORT || 3000
 
 http.listen(PORT, () => {
@@ -10,18 +33,43 @@ http.listen(PORT, () => {
 
 app.use(express.static(__dirname + '/public'))
 
+//routes
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
 })
 
-// Socket 
+// Socket connection
 const io = require('socket.io')(http)
 
-io.on('connection', (socket) => {
+
+io.on('connection', socket => {
     console.log('Connected...')
-    socket.on('message', (msg) => {
-       socket.broadcast.emit('message', msg)
+  //join notifiction data sender
+  let Room;
+  socket.on("join", userinfo => {
+    socket.join(userinfo.room)
+    console.log(socket.join)
+    Room = userinfo.room;
+    socket.broadcast.to(Room).emit('join', userinfo);
+  })
+
+// sending saved message 
+Message.find({},(err,data)=> {
+      if(!err){
+    socket.emit('saved-message', data);
+      }else{
+        console.log(err)
+      }
+    });
+
+    
+  // sending current message
+    socket.on('message',  msg => {
+       socket.broadcast.to(Room).emit('message', msg);
+       const message = new Message(
+         msg
+       );
+       message.save()
    })
 
 })
-
